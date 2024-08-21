1df4837687875b1d2df290e7e38eb1f7a222df31
pato2003 <patogabrielferreyra@gmail.com>
2024-08-21 15:30:35 -0300

    finalizacion de la simulacion de compra

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,12 @@
 let cartProducts = JSON.parse(localStorage.getItem("cartProducts"))
-console.log(cartProducts)
 const cartContainer = document.getElementById("cart-container");
 const clearButton = document.getElementById("clearCart");
 const totalContainer = document.getElementById("totalCompra");
-
-
+const btnBuy = document.getElementById("btnBuy")
 
 clear();
 renderCart(cartProducts);
+
 
 function renderCart(cartProducts) {
     if(cartProducts){
@@ -39,7 +38,6 @@ function clear() {
         cartProducts = [];
         calculoTotal();
     }
-    
 }
 
 function calculoTotal() {
@@ -91,6 +89,39 @@ function btnFunciones() {
             renderCart(cartProducts)
         }
     })
-
+    
+    btnBuy.onclick = async () =>{
+        const { value: formValues } = await Swal.fire({
+            title: "Rellene los apartados",
+            icon: "info",
+            html: `
+                <label for="nombre" class ="swal2-label">Nombre y apellido</label><br>
+                <input type="text" name="nombre" id="nombre" class="swal2-input"><br>
+                <label for="dni" class ="swal2-label">DNI</label><br>
+                <input type="number" name="dni" id="dni" class="swal2-input"><br>
+                <label for="mail" class ="swal2-label">E-mail</label><br>
+                <input type="email" name="mail" id="mail" class="swal2-input"><br>
+            `,
+            focusConfirm: false,
+            preConfirm: () => {
+              return [
+                document.getElementById("nombre").value,
+                document.getElementById("dni").value,
+                document.getElementById("mail").value
+              ];
+            }
+          });
+          if (formValues) {
+            Swal.fire({
+            icon: "success",
+            title: `Gracias por tu compra ${document.getElementById("nombre").value}!`,
+            text: `Te llegara un correo a ${document.getElementById("mail").value} con las indicaciones para efectuar la compra`,
+            });
+            localStorage.clear();
+            cartContainer.innerHTML = "";
+            cartProducts = [];
+            calculoTotal();
+          }
+    }
 
 }
